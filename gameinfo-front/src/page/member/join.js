c42aa9b2cbff1267cf937dea6a9e679ebea1ea5f
MoMoon-LKH <rlgh28@naver.com>
2022-07-19 15:21:38 +0900

    가입 관련 Api 연동

--- gameinfo-front/src/page/member/join.js
+++ gameinfo-front/src/page/member/join.js
@@ -1,47 +1,82 @@
 import './join.css'
+import * as Properties from '../../properties.js'
+import React, {useState} from "react";
+import axios from 'axios'
+
+function Join(){
 
 
-export function Join(){
+    const [inputs, setInput] = useState({
+        memberId: "",
+        password: "",
+        name: "",
+        nickname: "",
+        email: "",
+        phone: ""
+    });
+
+    const handleChange = e =>{
+        setInput({
+            ...inputs,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+
+        const url = Properties.API_URL + "/member/join";
+        const header = {
+            'Content-type': 'application/json',
+            'Accept': 'application/json'
+        }
+        
+        axios.post(url, inputs)
+            .then((result) => {
+                console.log(result.data)
+            })
+            .catch((e) => {
+                alert("가입에 실패");
+                console.log(e);
+            })
+        
+    }
     
     return (
         <div className="container">
-            <div class="join-form">
+            <form className="join-form" onSubmit={handleSubmit}>
                 <div className="join-div">
                     <div className="join-subtitle">아이디</div>
-                    <input className="join-input" type='text'/>
+                    <input name='memberId' value={inputs.memberId} onChange={handleChange}  className="join-input" type='text' min={7}/>
                 </div>
                 <div className="join-div">
                     <div className="join-subtitle">비밀번호</div>
-                    <input className="join-input" type='password'/>
+                    <input name='password' value={inputs.password} onChange={handleChange} className="join-input" type='password'/>
                 </div>
                 <div className="join-div">
                     <div className="join-subtitle">이름</div>
-                    <input className="join-input" type='text'/>
+                    <input name='name' value={inputs.name} onChange={handleChange} className="join-input" type='text'/>
                 </div>
                 <div className="join-div">
                     <div className="join-subtitle">닉네임</div>
-                    <input className="join-input" type='text'/>
+                    <input name='nickname' value={inputs.nickname} onChange={handleChange} className="join-input" type='text'/>
                 </div>
                 <div className="join-div">
                     <div className="join-subtitle">이메일</div>
-                    <input className="join-input" type='text'/>
+                    <input name='email' value={inputs.email} onChange={handleChange} className="join-input" type='text'/>
                 </div>
                 <div className="join-div">
                     <div className="join-subtitle">휴대전화</div>
-                    <input className="join-input" type='text'/>
+                    <input name='phone' value={inputs.phone} onChange={handleChange} className="join-input" type='text'/>
                 </div>
                 <div className="join-div">
                     <div className="join-button-div">
-                        <button>가입하기</button>
+                        <button type='submit'>가입하기</button>
                     </div>
                 </div>
-            </div>
+            </form>
         </div>
     )
 }
 
 export default Join;
-
-function ajaxJoin(props){
-    
-}
--- gameinfo-front/src/properties.js
+++ gameinfo-front/src/properties.js
@@ -1,2 +1,6 @@
 
-const API_URL = "http://localhost:8080";
+
+
+export const API_URL = "http://localhost:8080/api";
+ 
+export default API_URL;
